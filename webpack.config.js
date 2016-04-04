f3a837046145bb6270e176318bbdf6d6eb8a5244
oknoorap <oknoorap@djavaweb.com>
2016-04-04 10:37:08 +0700

    Fix node env

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     }
 }
 
-if (process.env.NODE_ENV === 'productiond') {
+if (process.env.NODE_ENV === 'production') {
     module.exports.plugins = [
     new webpack.DefinePlugin({
         'process.env': {
