b635e8fdfc528282788c2c91726a077aa89a6b68
oknoorap <oknoorap@gmail.com>
2016-10-24 10:13:06 +0700

    Move component to example pages.
    Everyone can build a component, so these basic components are just example.

--- example/my-builder.js
+++ example/my-builder.js
@@ -1,5 +1,5 @@
 uno.loadCanvas({
-    url: './example/index.html',
+    url: './example/page.html',
     element: '.entry-content'
 }).on('ready', function () {
 
@@ -8,18 +8,18 @@ uno.loadCanvas({
      */
 
     // Grid components
-    uno.addComponent('../components/column-1/component.js')
-    uno.addComponent('../components/column-2/component.js')
-    uno.addComponent('../components/column-3/component.js')
-    uno.addComponent('../components/column-4/component.js')
-    uno.addComponent('../components/column-5/component.js')
-    uno.addComponent('../components/column-6/component.js')
+    uno.addComponent('./example/components/column-1/component.js')
+    uno.addComponent('./example/components/column-2/component.js')
+    uno.addComponent('./example/components/column-3/component.js')
+    uno.addComponent('./example/components/column-4/component.js')
+    uno.addComponent('./example/components/column-5/component.js')
+    uno.addComponent('./example/components/column-6/component.js')
 
     // Basic component
-    uno.addComponent('../components/text/component.js')
-    uno.addComponent('../components/heading/component.js')
-    uno.addComponent('../components/button/component.js')
-    uno.addComponent('../components/image/component.js')
-    uno.addComponent('../components/video/component.js')
-    uno.addComponent('../components/map/component.js')
+    uno.addComponent('./example/components/text/component.js')
+    uno.addComponent('./example/components/heading/component.js')
+    uno.addComponent('./example/components/button/component.js')
+    uno.addComponent('./example/components/image/component.js')
+    uno.addComponent('./example/components/video/component.js')
+    uno.addComponent('./example/components/map/component.js')
 })
