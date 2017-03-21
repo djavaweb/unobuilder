411914ae21602d5d2364d3b58d0cdb33c903791c
Muhammad Habib Rohman <mhrohman@live.com>
2017-03-21 10:01:36 +0700

    fix cannot set gutter wrong key map when set domProps.gutter

--- src/store/modules/elements.js
+++ src/store/modules/elements.js
@@ -323,7 +323,7 @@ const mutations = {
         const value = obj[utils.attrType.KIND]
         classes[utils.GlobalClassName(value)] = true
         if (value === 'row') {
-          obj.domProps.gutter = {}
+          obj.dataObject.domProps['gutter'] = {}
         }
       }
 
