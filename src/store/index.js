1b0027c8813029a008cf640af574ea037e9eaac7
Muhammad Habib Rohman <mhrohman@live.com>
2017-03-24 14:25:23 +0700

    Refactor element offsets
    Add dropline functionalities

--- src/store/index.js
+++ src/store/index.js
@@ -46,8 +46,63 @@ const actions = {
   }
 }
 
+const getters = {
+  /**
+   * Selected element offset
+   * @param {Object} state
+   * @return {Object}
+   */
+  elementOffset (state, getter) {
+    const NodeHelpers = new NodeUtils(state.elements)
+    const getOffset = element => {
+      const _element = NodeHelpers.getElementNodeById(element.id)
+
+      if (!_element) {
+        return {}
+      }
+
+      let {top, left, width, height} = _element.getBoundingClientRect()
+
+      if (left < 0) {
+        width += left
+        left = 0
+      }
+
+      if (getter.canvasScroll.top) {
+        top += Math.abs(getter.canvasScroll.top)
+        left += Math.abs(getter.canvasScroll.left)
+      }
+
+      return { top, left, width, height }
+    }
+
+    let selected
+    let hovered
+
+    if (state.elements.selected) {
+      selected = getOffset(state.elements.selected)
+    }
+
+    if (state.elements.hovered) {
+      hovered = getOffset(state.elements.hovered)
+      if ((state.components.dragging.status || state.elements.dragging.status) && state.elements.dropline.position.bottom) {
+        const parent = NodeHelpers.getRealParent(state.elements.hovered.id)
+        if (parent) {
+          hovered = getOffset(parent)
+        }
+      }
+    }
+
+    return {
+      selected,
+      hovered
+    }
+  }
+}
+
 export default new Vuex.Store({
   actions,
+  getters,
   modules: {
     styles,
     panels,
