cfd4127906ddd8b04d04627a3e1b95d5f4f69479
Muhammad Habib Rohman <mhrohman@live.com>
2017-05-16 14:22:53 +0700

    fix(build)

--- dist/example/my-builder.js
+++ dist/example/my-builder.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 uno.builder('#uno-builder').loadCanvas({
-  url: '/example/page.html',
+  url: '/unobuilder/example/page.html',
   element: '.entry-content'
 }).on('ready', function () {
   /**
@@ -9,22 +9,22 @@ uno.builder('#uno-builder').loadCanvas({
    */
 
   // Basic Blocks
-  uno.addBlock('/example/blocks/section.uno')
-  uno.addBlock('/example/blocks/section-container.uno')
+  uno.addBlock('/unobuilder/example/blocks/section.uno')
+  uno.addBlock('/unobuilder/example/blocks/section-container.uno')
 
   // Grid components
-  uno.addComponent('/example/components/column-1.uno')
-  uno.addComponent('/example/components/column-2.uno')
-  uno.addComponent('/example/components/column-3.uno')
-  uno.addComponent('/example/components/column-4.uno')
-  uno.addComponent('/example/components/column-5.uno')
-  uno.addComponent('/example/components/column-6.uno')
+  uno.addComponent('/unobuilder/example/components/column-1.uno')
+  uno.addComponent('/unobuilder/example/components/column-2.uno')
+  uno.addComponent('/unobuilder/example/components/column-3.uno')
+  uno.addComponent('/unobuilder/example/components/column-4.uno')
+  uno.addComponent('/unobuilder/example/components/column-5.uno')
+  uno.addComponent('/unobuilder/example/components/column-6.uno')
 
   // Basic component
-  uno.addComponent('/example/components/text.uno')
-  uno.addComponent('/example/components/heading.uno')
-  uno.addComponent('/example/components/button.uno')
-  uno.addComponent('/example/components/image.uno')
-  uno.addComponent('/example/components/video.uno')
-  uno.addComponent('/example/components/map.uno')
+  uno.addComponent('/unobuilder/example/components/text.uno')
+  uno.addComponent('/unobuilder/example/components/heading.uno')
+  uno.addComponent('/unobuilder/example/components/button.uno')
+  uno.addComponent('/unobuilder/example/components/image.uno')
+  uno.addComponent('/unobuilder/example/components/video.uno')
+  uno.addComponent('/unobuilder/example/components/map.uno')
 })
